8383e0264488847aa6079720dec0c18f5df1e911
Lucas Ribeiro <lucasribeiro5569@gmail.com>
2022-05-02 15:37:12 -0300

    .

--- plugins/simsimiApi.js
+++ plugins/simsimiApi.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 module.exports = {
     sendMessageSimSimi: async (text) => {
-        const response = await axios.get("https://api.simsimi.net/v1", {
+        const response = await axios.get("https://api.simsimi.net/v2", {
         params: {lang: 'pt', text: text}
     })
     return response.data["success"];
